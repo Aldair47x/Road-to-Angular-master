993e0c848b64f8a16af7362bc5ba66243c518a6a
Aldair47x <aldair47x@utp.edu.co>
2021-07-27 14:27:22 -0500

    Heroes app primera parte

---
+++ heroesApp/src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { E404Component } from './shared/e404/e404.component';
+
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule )
+  },
+  {
+    path: 'heroes',
+    loadChildren: () => import('./heroes/heroes.module').then(m => m.HeroesModule )
+  },
+  {
+    path: '404',
+    component: E404Component
+  },
+  {
+    path: '**',
+    redirectTo: '404'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
